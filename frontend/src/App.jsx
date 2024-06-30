c0282d266d812e6cae3a7033eb04dc672424566f
ysskrishna <sivasaikrishnassk@gmail.com>
2024-06-30 21:45:12 +0530

    Siva | update landing page design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import config from './config';
 import axios from 'axios';
 import useWebSocket from 'react-use-websocket';
+import Table from './components/Table';
+import Upload from './components/Upload';
 
 
 const App = () => {
@@ -70,25 +72,24 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Image to Video Converter</h1>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload} disabled={!file || uploading}>
-        Upload
-      </button>
+    <div className="max-w-6xl mx-auto p-6 my-2">
+      <section class="bg-white dark:bg-gray-900">
+        <div class="px-4 mx-auto max-w-screen-xl text-center">
+            <h1 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-4xl lg:text-5xl">Image to Video Generator</h1>
+            <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-16">Transform your images into stunning  videos in just a few clicks. Start creating your video now and share your story with the world.</p>
+        </div>
+      </section>
+
+      <div class="flex flex-col items-center justify-center w-full">
+        <Upload file={file} handleFileChange={handleFileChange} />
+        <button onClick={handleUpload} disabled={!file || uploading} class="w-[100%] disabled:opacity-50 disabled:cursor-not-allowed my-2 cursor-pointer relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+          <span class="w-[100%] relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            Generate Video
+          </span>
+        </button>
+      </div> 
       
-      <br />
-      <h2>Task List</h2>
-      <ul>
-        {tasks.map(task => (
-          <li key={task?.task_id}>
-            Task ID: {task?.task_id}, Image URL: {config?.baseUrl}/{task?.image_url}, Status: {task?.status}, Progress: {task?.progress}%
-            {task.status === 'success' && (
-              <a href={`${task?.video_url}`} target="_blank" rel="noopener noreferrer">View Video</a>
-            )}
-          </li>
-        ))}
-      </ul>
+      <Table data={tasks} />
     </div>
   );
 };
